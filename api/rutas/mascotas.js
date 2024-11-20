a6969b507b9805f87010fc17ca27b42fec72e458
nitram <164651909+lenrit@users.noreply.github.com>
2024-11-19 21:58:39 -0300

    Add files via upload

--- api/rutas/mascotas.js
+++ api/rutas/mascotas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload=require('../config/cloudinary_config');
+const upload=require('../middlewares/multer');
 //Importamos controlador
 const controladorMascota = require('../controllers/mascotasController')
 // import {multerUpload} from '../middlewares/multer'
@@ -9,7 +9,7 @@ const multerUpload = require('../middlewares/multer');
 // localhost:3000/mascotas/
 router.get('/', controladorMascota.getAllMascotas);
 router.get('/:id', controladorMascota.getMascotaById);
-router.post('/registro',multerUpload.single('file'), controladorMascota.crearMascotas);
+router.post('/registro',upload.single('file'), controladorMascota.crearMascotas);
 router.put('/:id', controladorMascota.actualizarMascota);
 router.delete('/:id', controladorMascota.eliminarMascota);
 
