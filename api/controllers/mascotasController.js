c4fb8630180293c5528a93b0af03d66d43080085
solmsl <mejiasilesol@gmail.com>
2024-11-20 00:55:15 -0300

    corrijo ruta

--- api/controllers/mascotasController.js
+++ api/controllers/mascotasController.js
@@ -1,6 +1,6 @@
 const { body, validationResult } = require('express-validator');
 const mascotas = require('../models/modelMascotas');
-const cloudinary = require('../config/cloudinary');
+const cloudinary = require('../config/cloudinary_config');
 
 console.log (cloudinary ? 'anda' : 'no anda cloduinary');
 const fs = require('fs');
